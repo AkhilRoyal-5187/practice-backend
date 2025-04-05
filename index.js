6df0c6456ac78246fd53d48fb6003407f5801d41
AkhilRoyal-5187 <akhilroyal@5187>
2025-04-05 10:19:54 +0530

    this is the commit for the index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = 3000
 
-app.listen(port, ()=>{
+app.listen(process.env.port, ()=>{
     console.log(`hello world ${port} `);
 })
 
