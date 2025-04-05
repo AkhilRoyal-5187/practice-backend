47530091b759971fd5bb75412990a60975ce2a35
AkhilRoyal-5187 <akhilroyal@5187>
2025-04-05 08:26:14 +0530

    my try commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+
+const express = require("express");
+const app = express();
+const port = 3000
+
+app.listen(port, ()=>{
+    console.log(`hello world ${port} `);
+})
+
+app.get("/" , (req,res)=>{
+    res.send("hello this is the defaulft page");
+})
